7863bcdf5c92a1c88dc950c5ee1ba0d645406994
Chidalu Agbakwa <cagbakwa@gmail.com>
2021-04-12 22:30:53 -0400

    FIxed missScraper to be able to go to any link now

--- src/util/missScraper.ts
+++ src/util/missScraper.ts
@@ -4,31 +4,57 @@ import * as LinkUtil from './LinkUtils';
 import * as fwCoursesData from '../../data/fwCoursesCopy.json';
 import CourseFacultyOld from '../interfaces/CourseFacultyOld';
 import CourseToCourseFacultyOld from '../interfaces/CourseToCourseFacultyOld';
+import PageData from '../interfaces/PageData';
 
-
+/**
+ * In the case the db does not have the course inside it, we need to rescrape the york website and grab the page info
+ * 
+ * @param courseFaculty 
+ * @returns {PageData} - A Obj Representing all the data on the page
+ */
 const grabMissedCourse = async (courseFaculty: CourseFacultyOld): Promise<void> => {
-   let {data}: AxiosResponse<any> = await axios.get(LinkUtil.getRootYorkLink(courseFaculty, "fw"));
-   let $: cheerio.Root = cheerio.load(data);
-   const tBodySelector: string = "body > table > tbody > tr:nth-child(2) > td:nth-child(2) > table > tbody > tr:nth-child(2) > td > table > tbody > tr > td > table:nth-child(3) > tbody";
-   const courseNum: string = "2030";
-   let link: string = "";
-   $(tBodySelector).each((idx:number, ele:cheerio.Element) => { 
-       if($(ele).find('td').text().includes("EECS 2030")) {
-        link = $(ele).find('td:nth-child(3) > a').attr('href') || "";
-       }
-   })
+    //console.log(LinkUtil.getRootYorkLink(courseFaculty, "fw"));
+    let { data }: AxiosResponse<any> = await axios.get(LinkUtil.getRootYorkLink(courseFaculty, "fw"));
+    let $: cheerio.Root = cheerio.load(data);
+    const tBodySelector: string = "body > table > tbody > tr:nth-child(2) > td:nth-child(2) > table > tbody > tr:nth-child(2) > td > table > tbody > tr > td > table:nth-child(3) > tbody";
+    const courseNum: string = "2030";
+    let link: string = "";
 
-   if(link) {
-       link = LinkUtil.getFullSpecificCourseURL(link);
-   }
-
-   const specificCoursePage: AxiosResponse<any> = await axios.get(link);
-   $ = cheerio.load(specificCoursePage.data);
-
+    //SELECT ALL tr's that are DESCENDANTS of tBody
+    $(tBodySelector + " > tr").each(function (i, ele) {
+        if($(ele).children().first().text().includes("EECS 2030")) {
+            //console.log($(ele).find('td:nth-child(3)').html());
+            //console.log($($(ele).find('td:nth-child(3)').html()).attr('href'));
+            //Messy way of getting the href, might need to fix later
+            link = $($(ele).find('td:nth-child(3)').html()).attr('href') || "";
+        }
+    })
 
 
+    if (link) {
+        link = LinkUtil.getFullSpecificCourseURL(link);
+        console.log(link);
+    }
+    else {
+        return;
+    }
 
-   console.log(link);
+    const specificCoursePage: AxiosResponse<any> = await axios.get(link);
+    $ = cheerio.load(specificCoursePage.data);
+
+    const courseNameSelector: string = "body > table > tbody > tr:nth-child(2) > td:nth-child(2) > table > tbody > tr:nth-child(2) > td > table > tbody > tr > td > table:nth-child(1) > tbody > tr > td:nth-child(1) > h1";
+    const courseDescriptionSelector: string = "body > table > tbody > tr:nth-child(2) > td:nth-child(2) > table > tbody > tr:nth-child(2) > td > table > tbody > tr > td > p:nth-child(5)";
+
+    const result: PageData = {
+        courseName: "",
+        courseDescription: "",
+        pageTableData: []
+    };
+
+    result.courseName = $(courseNameSelector).text();
+    result.courseDescription = $(courseDescriptionSelector).text();
+
+    console.log(result.courseName, result.courseDescription);
     return;
 }
 
